3b8144f15e17e6499e883fc80f6f4c79c3b53c67
Kenzein <156581853+Kenzein@users.noreply.github.com>
2025-07-01 16:13:41 +0700

    Menambahkan hambuger menu dan efek hover navigasi

--- js/script.js
+++ js/script.js
@@ -8,14 +8,11 @@ function showRegis() {
   hideLogin();
   registerSection.classList.add("active");
 }
-// Menghilangkan regis
-function hideRegis() {
-  registerSection.classList.remove("active");
-}
 
 // Klik pada Link Register
 registerLink.addEventListener("click", function (e) {
   e.preventDefault();
+  hideHamburgerMenu();
   if (registerSection.classList.contains("active")) {
     hideRegis(); // Kondisi hide menyala
   } else {
@@ -43,13 +40,10 @@ function showLogin() {
   loginSection.classList.add("active");
 }
 
-// Menyembunyikan login
-function hideLogin() {
-  loginSection.classList.remove("active");
-}
 // Klik pada Link Login
 loginLink.addEventListener("click", function (e) {
   e.preventDefault(); // stay di halaman yang sama
+  hideHamburgerMenu();
   if (loginSection.classList.contains("active")) {
     hideLogin(); // Kondisi hide menyala
   } else {
@@ -65,14 +59,14 @@ loginSection.addEventListener("click", (event) => {
   }
 });
 // Login End
-//Other
+//Other start
 const otherSections = document.querySelectorAll(
   "#home, #about, #action, #news"
 );
 const otherLinks = document.querySelectorAll(
   '.navbar-nav a[href="#home"], .navbar-nav a[href="#about"], .navbar-nav a[href="#action"], .navbar-nav a[href="#news"]'
 );
-// Menambahkan event listener untuk setiap section
+// Menambah event listener untuk setiap section
 otherSections.forEach((section) => {
   section.addEventListener("click", () => {
     hideRegis(); // Sembunyikan registrasi
@@ -80,7 +74,7 @@ otherSections.forEach((section) => {
   });
 });
 
-// Menambahkan peristiwa listener untuk setiap tautan di navbar
+// Menambah event listener untuk semua tautan di navbar
 otherLinks.forEach((link) => {
   link.addEventListener("click", (e) => {
     e.preventDefault(); //stay dihalaman yang sama
@@ -95,3 +89,43 @@ otherLinks.forEach((link) => {
       .scrollIntoView({ behavior: "smooth" });
   });
 });
+//Other end
+// Hamburger menu start
+const hamburger = document.getElementById("hamburger-menu");
+const navbarNav = document.querySelector(".navbar-nav");
+// Ketika click menu
+hamburger.addEventListener("click", () => {
+  navbarNav.classList.toggle("active");
+
+  if (navbarNav.classList.contains("active")) {
+    hideAllPops();
+  }
+});
+// Ketika klik diluar menu
+document.addEventListener("click", (e) => {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    hideHamburgerMenu();
+  }
+});
+
+// Hamburger menu end
+
+// Kumpulan function hide
+// Menghilangkan navigasi menu
+function hideHamburgerMenu() {
+  navbarNav.classList.remove("active");
+}
+
+// Menghilangkan login
+function hideLogin() {
+  loginSection.classList.remove("active");
+}
+// Menghilangkan regis
+function hideRegis() {
+  registerSection.classList.remove("active");
+}
+
+function hideAllPops() {
+  hideLogin();
+  hideRegis();
+}
